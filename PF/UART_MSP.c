2775db95152fbdebd9ccaffd8bd7bb8de1ca4632
Fruffylol <161537806+Fruffylol@users.noreply.github.com>
2024-07-04 23:43:00 -0600

    Add files via upload

---
+++ PF/UART_MSP.c
@@ -0,0 +1,37 @@
+#include <msp430.h>
+
+char cadena[] = "Hola desde UART2";
+volatile char i =0;
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P1DIR |= BIT0;                            //declaramos el led como salida
+  //Configuracion de la UART
+  P3SEL |= BIT3+BIT4;                       // P3.3,4 = USCI_A0 TXD/RXD
+  UCA0CTL1 |= UCSWRST;                      // Put state machine in reset
+  UCA0CTL1 |= UCSSEL_1;                     // SMCLK
+  UCA0BR0 = 3;                              // 1MHz 9600
+  UCA0BR1 = 0;                              // 1MHz 9600
+  UCA0MCTL |= 0x06;            // Modulation UCBRSx=1, UCBRFx=0
+  UCA0CTL0 &= 0x0;
+  UCA0CTL1 &= ~UCSWRST;
+  // Initialize USCI state machine
+  //Configuracion del Boton P2.1
+  P2DIR &= ~BIT1; //Configuro P2.1 como entrada
+  P2REN |= BIT1; //Activa la resistencia P2.1
+  P2OUT |= BIT1; //Conecto a Vcc la resistencia/Envio un logico P2.1
+
+  while(1){
+      i =0;
+      if ((!(P2IN & BIT1))){ //Si presiono se activa
+          while(cadena[i] != '\0'){
+              UCA0TXBUF = cadena[i];
+              while(UCA0STAT & UCBUSY);
+              i++;;
+              __delay_cycles(100000);
+          }
+      }else{
+          P1OUT |= BIT0;
+      }
+  }
+}
